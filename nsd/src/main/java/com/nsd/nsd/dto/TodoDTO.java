72e5df0050af372367a0cd44b4bbd479e4771fc5
yeongseok-yun <104549298+yeongseok-yun@users.noreply.github.com>
2022-08-07 22:13:39 +0900

    Add files via upload
    commit

---
+++ nsd/src/main/java/com/nsd/nsd/dto/TodoDTO.java
@@ -0,0 +1,24 @@
+package com.nsd.nsd.dto;
+
+import com.nsd.nsd.model.TodoEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO {
+	private String id;
+	private String title;
+	private Boolean done;
+	
+	public TodoDTO(final TodoEntity entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.done = entity.isDone();
+	}
+}
---
+++ nsd/src/test/java/com/nsd/nsd/NsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsd.nsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
