6cf8d5d17b44f7814b6fb97ffd4a2cfa33c354be
yeongseok yun <antiravesol@gmail.com>
2022-08-08 22:35:24 +0900

    테스트 컨트롤러 커밋

---
+++ nsd/src/main/java/com/nsd/nsd/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.nsd.nsd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+	@GetMapping
+	public String testController() {
+		return "welcome test controller";
+	}
+}
