b2f6fcbba60291119d8c981e3962056c78258a6e
antir <antir@YUN>
2022-08-11 23:46:23 +0900

    RequestParam 테스트 메소드 추가

--- nsd/src/main/java/com/nsd/nsd/controller/TestController.java
+++ nsd/src/main/java/com/nsd/nsd/controller/TestController.java
@@ -3,6 +3,7 @@ package com.nsd.nsd.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,10 @@ public class TestController {
 	@GetMapping("test/{id}")//localhost:8080/test/test/33
 	public String testGetPath(@PathVariable(required = false)int id) {
 		return "id : "+id;
+	}
+	
+	@GetMapping("paramTest")//localhost:8080/test/paramTest?val_ue=hello
+	public String paramTestMethod(@RequestParam String val_ue) {
+		return "param :" + val_ue;
 	}
 }
